c2a1cb6ce22d00f29f757caddc0d2685e33f0465
larasatizka <larasatizka@gmail.com>
2020-02-18 20:31:17 +0700

    Perubahan telah dilakukan

--- app/src/main/java/id/ac/polinema/intent/ImplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ImplicitIntentActivity.java
@@ -1,11 +1,19 @@
 package id.ac.polinema.intent;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
 
 public class ImplicitIntentActivity extends AppCompatActivity {
 
@@ -24,5 +32,25 @@ public class ImplicitIntentActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_CANCELED){
+            return;
+        }
+        if(requestCode==GALLERY_REQUEST_CODE){
+            if(data!=null){
+                try{
+                    Uri imageUri=data.getData();
+                    Bitmap bitmap=MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    avatarImage.setImageBitmap(bitmap);
+                } catch (IOException e){
+                    Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG,e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void handleChangeAvatar(View view) {
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
     }
 }
--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -22,4 +22,9 @@ public class MainActivity extends AppCompatActivity {
     public void handleExit(View view) {
         finish();
     }
+
+    public void handleImplicitIntennt(View view) {
+        Intent intent = new Intent(this, ImplicitIntentActivity.class);
+        startActivity(intent);
+    }
 }
